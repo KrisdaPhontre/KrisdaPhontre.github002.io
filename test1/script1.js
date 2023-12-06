d77adf56a9cad76eef67019c65fe4f168eaab0e3
KrisdaPhontre <s66309010002@kktech.ac.th>
2023-12-06 19:47:20 +0700

    Create script1.js

---
+++ test1/script1.js
@@ -0,0 +1,30 @@
+function myFunction(am) {
+    am.classList.toggle('change')
+}
+
+let acc = document.getElementsByClassName('accordion');
+let i;
+
+for (i = 0 ; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle('active');
+
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null}
+        else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+
+    })
+}
+
+function myFunction() {
+    let x = document.getElementById("mylink");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
