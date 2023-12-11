f74cd96dd5cf43f2b9938d28b738360776f9facf
KrisdaPhontre <s66309010002@kktech.ac.th>
2023-12-11 20:39:08 +0700

    Update script.js

--- My Website/script.js
+++ My Website/script.js
@@ -1,39 +1,21 @@
 function openNav() {
-    document.getElementById("myNav").style.display = "block";
-}
-  
-  function closeNav() {
-    document.getElementById("myNav").style.display = "none";
+    document.getElementById("myNav").style.width = "100%";
 }
 
-function myFunction(am) {
-    am.classList.toggle('change')
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
 }
 
-let acc = document.getElementsByClassName('accordion');
-let i;
-
-for (i = 0 ; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle('active');
-
-        let panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null}
-        else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-
-    })
-}
-
-    function myFunction() {
-        let x = document.getElementById("mylink");
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        }
-        else {
-            x.style.display = "block";
-        }
+function myFunction() {
+    let x = document.getElementById("mylink");
+    if (x.style.display === "block") {
+        x.style.display = "none";
     }
+    else {
+        x.style.display = "block";
+    }
+}
 
+function myFunction(x) {
+  x.classList.toggle("change");
+}
